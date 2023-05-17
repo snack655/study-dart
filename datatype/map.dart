4d478fa973404a54f1006ada9e12b07036a4c513
최민재 <cmjcmj1230@gmail.com>
2023-05-17 16:52:57 +0900

    Study Map and Set

---
+++ datatype/map.dart
@@ -0,0 +1,19 @@
+void main() {
+  var player = {
+    'name': 'nico',
+    'xp': 19.99,
+    'superpower': false,
+  };
+
+  var player2 = {
+    'name': 'nico',
+    'xp': '19.99',
+    'superpower': 'false',
+  };
+
+  Map<int, bool> player3 = {};
+  Map<int, bool> player4 = {
+    1: true,
+    2: false,
+  };
+}
